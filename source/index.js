871fad1bb06e4de0fb873b66ce3e5e5dc0a9c0eb
Dan Li <ardanhk@gmail.com>
2014-11-08 19:23:45 +0800

    Update Directory Name

--- source/index.js
+++ source/index.js
@@ -1,32 +1,57 @@
 var express = require('express'),
 bodyParser = require('body-parser'),
 path = require('path'),
-MongoClient = require('mongodb').MongoClient,
-Server = require('mongodb').Server,
-CollectionDriver = require('./collectionDriver').CollectionDriver;
+passport = require('passport'),
+LocalStrategy = require('passport-local');
 
 var app = express();
 app.set('port', process.env.PORT || 8080);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+app.use(passport.initialize());
+app.use(passport.session());
 
-var mongoHost = 'localHost';
-var mongoPort = 27017;
-var collectionDriver;
-
-MongoClient.connect('mongodb://localhost/hkciegu', function(error, db) {
-	if (error) {
-		console.error("Error! Exiting... Must start MongoDB first");
-		process.exit(1);
+passport.use(new LocalStrategy( function(username, password, done) {
+	console.log("Username: " + username + " Password: " + password)
+	if (username == "test" && password == "test") {
+		done(null, {username: "test"});
+	} else {
+		return done("{message: 'Incorrect username.'}");
 	}
-	console.log("Successfully connected to the database");
-	collectionDriver = new CollectionDriver(db);
-	//db.close();
+}));
+
+passport.serializeUser(function(user, done) {
+	console.log("Serialize User: " + user);
+  	done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+	console.log("Deserialize User: " + user);
+  	done(null, user);
+});
+
+app.post('/login', function(req, res, next) {
+	passport.authenticate('local', function (error, user, info) {
+		if (error) {
+			console.log(error);
+			return res.redirect('/login.html');
+		}
+		if (!user) {
+			console.log("User is null");
+			return res.redirect('/login.html');
+		}
+		return res.redirect('/admin');
+	})(req, res, next);
 });
 
 app.all('/admin/*', function(req, res, next) {
-	
-	next();
+	if (req.isAuthenticated()) {
+		console.log('next()');
+		next();
+	} else {
+		console.log('redirect to login');
+		res.redirect('/login.html');
+	}
 });
 
 app.use('/admin', express.static(path.join(__dirname, 'admin')));
@@ -34,3 +59,11 @@ app.use('/admin', express.static(path.join(__dirname, 'admin')));
 
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(8080);
+
+/*
+http.createServer(app).listen(app.get('port'), function() {
+	console.log('Express server listening on port ' + app.get('port'));
+})
+*/
