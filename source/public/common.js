948f333d5e0f8fec6527be4f5bb8b56c1695f85c
YDLau <tears.dl@gmail.com>
2014-11-17 10:35:33 +0800

    font-size & restructure
    1. Separate footer & menu
    2. added common.js for font size
    3. body level font size changing
    4. restructure header/footer/menu for all pages

---
+++ source/public/common.js
@@ -0,0 +1,9 @@
+
+	
+		function changeFontSize(option){
+			$("body").removeClass('fontSmall');
+			$("body").removeClass('fontNormal');
+			$("body").removeClass('fontMiddle');
+			$("body").removeClass('fontLarge');
+			$("body").addClass(option);
+		}
